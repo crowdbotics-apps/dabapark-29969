bedae2b3d3584d8f04a6946bffb42da316fe07fd
Crowdbotics <team@crowdbotics.com>
2021-08-20 11:34:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "dabapark_29969Identifier",
+  url: "https://dabapark_29969Identifier.botics.co",
+  api: "https://dabapark_29969Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
